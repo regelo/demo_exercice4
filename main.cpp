42e69fbfca8a2448754045dcfc9c64740497fdf9
regelo <regelo@regelo.org>
2021-04-29 13:04:49 -0400

    Premier commit de mon projet d'exercice 4

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+    int entiers[] = {0, 5, 10, 15, 20};
+    int* pointeur = entiers;
+
+    //*(pointeur+0) = *(pointeur+0) + 2;
+    //*(pointeur+1) = *(pointeur+1) + 2;
+
+    for(int i=0 ; i<5 ; i++) {
+        *(pointeur+i) = *(pointeur+i) + 2;
+    }
+
+    for (int i=0 ; i<5 ; i++) {
+        std::cout << *(pointeur+i) << " ";
+    }
+}
